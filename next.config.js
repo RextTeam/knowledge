41490e1a0f7a1dcd8ae4faeb10fb2650116d85ee
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-27 17:45:07 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,5 +10,7 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   // Append the default value with md extensions
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    "images.unoptimized": false,
+    images: {
+        unoptimized: false,
+    },
 })
