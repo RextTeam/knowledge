f6fcda64cb7cf8a90a9476bfaefbd1250598df08
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 05:18:56 +0900

    Update [slug].tsx

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -2,6 +2,7 @@ import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import fs from 'fs'
 import path from 'path'
+import matter from 'gray-matter'
 
 
 type Param = {
