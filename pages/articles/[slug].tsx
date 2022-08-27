5431e409ceebc42b3054041e0f1959a726fe962a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 04:57:41 +0900

    Update [slug].tsx

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -6,6 +6,16 @@ export async fucntion getStaticPaths() {
     let files = fs.readdirSync(path.join("pages/articles"))
     files = files.filter((file) => file.split(".")[1] === "mdx");
     const paths = files.map(file => {
-        return file.replace(".mdx", "")
+        return {
+            params: { slug: file.replace(".mdx", "") }
+        }
     })
+    return {
+        paths: paths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps() {
+    
 }
