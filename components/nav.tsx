03a1997dd331ecfff775ac60dcef2fa8c24561db
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 03:48:56 +0900

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -13,14 +13,17 @@ export default function Nav() {
         }
     ]
     return (
-        <div className="border-b-4 border-">
+        <div className="border-b-4">
             <div className="flex mx-8 justify-between my-2 items-center">
                 <div>
                     <Link href="/"><a><h1 className="text-4xl">Knowledge</h1></a></Link>
                 </div>
-                <div>
-                    {Items.map(item => <a className="text-3xl mx-4" href={item.url} key={item.name}>{item.name}</a>}
-                </div>
+                <ul className="flex">
+                    {Items.map(item => (
+                        <li className="mx-4" key={item.name}>
+                            <a className="text-3xl" href={item.url} key={item.name}>{item.name}</a>
+                        </li>
+                </ul>
             </div>
         </div>
     )
