04b68fb5b7fad956f2070e4b81e579d2bb9b096a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-27 18:09:12 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from '../components/head'
+import Nav from '../components/nav'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <>
           <Head />
           <body>
-              <Component {...pageProps} />
+              <Nav />
+              <main>
+                  <Component {...pageProps} />
+              </main>
           </body>
       </>
   )
