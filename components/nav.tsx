e68a21f5df991bbdf40a6c3a5658c8ddb790f565
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 03:43:53 +0900

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,6 +2,16 @@ import Link from 'next/link'
 
 
 export default function Nav() {
+    const Items = [
+        {
+            name: "Website",
+            url: "https://rt.rest.dev"
+        },
+        {
+            name: "Docs",
+            url: "https://docs.rt.rext.dev"
+        }
+    ]
     return (
         <div className="border-b-4 border-">
             <div className="flex mx-8 justify-between my-2 items-center">
@@ -9,8 +19,7 @@ export default function Nav() {
                     <Link href="/"><a><h1 className="text-4xl">Knowledge</h1></a></Link>
                 </div>
                 <div>
-                    <a className="text-3xl mx-2" href="https://rt.rext.dev">Website</a>
-                    <a className="text-3xl mx-2" href="https://docs.rt.rext.dev">Docs</a>
+                    {Items.map(link => <a className="text-3xl mx-2" href={item.url}>{item.name}</a>}
                 </div>
             </div>
         </div>
