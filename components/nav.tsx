a2fc4e20a46be3a3ef36b84987f632bc1224ef6e
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 03:37:56 +0900

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -9,8 +9,8 @@ export default function Nav() {
                     <Link href="/"><a><h1 className="text-4xl">Knowledge</h1></a></Link>
                 </div>
                 <div>
-                    <a className="text-3xl" href="https://rt.rext.dev">Website</a>
-                    <a className="text-3xl" href="https://docs.rt.rext.dev">Docs</a>
+                    <a className="text-3xl mx-2" href="https://rt.rext.dev">Website</a>
+                    <a className="text-3xl mx-2" href="https://docs.rt.rext.dev">Docs</a>
                 </div>
             </div>
         </div>
