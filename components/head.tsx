6aa2a267544f3767bf7b19135ac26e529d599bdd
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-27 17:55:01 +0900

    Create head.tsx

---
+++ components/head.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+
+export default function Head() {
+    return (
+        <Head>
+            <title>Rextナウレッジ</title>
+            <meta property='og:title' content='Rextナウレッジ' />
+            <meta property='og:description' content='Rextたちのブログサイト。' />
+            <meta property='og:type' content='article' />
+        </Head>
+    )
+}
