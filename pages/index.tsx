e28489db6a9a12f1bef7a220561d2a5936a0d4c3
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 11:29:34 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export async function getStaticProps() {
             }
         })
     )
-    articles = [...articles].sort((a, b) => new Date(b) - new Date(a))
+    articles = [...articles].sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
     return {
         props: {
             articles: articles,
