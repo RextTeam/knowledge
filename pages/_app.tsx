63a8a5fd74ca1cbbbdac1c2824b58e564969d353
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 12:13:23 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,8 +12,8 @@ function MyApp({ Component, pageProps }: AppProps) {
               <main className="px-7 pt-10 grow">
                   <Component {...pageProps} />
               </main>
-              <footer>
-                  <small className="p-5 text-center text-xs">&copy; RextTeam 2022</small>
+              <footer className="justify-center>
+                  <small className="py-5 text-center text-xs">&copy; RextTeam 2022</small>
               </footer>
           </body>
       </>
