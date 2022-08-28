72861f56e8c0f282152531201ba3d75c74971aa2
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 10:52:35 +0900

    Update author.tsx

--- components/author.tsx
+++ components/author.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 const authors = {
     mc_fdc: {
         url: "https://mc-fdc.com",
-        introduce: "式守さん好き"
     }
 }
 
@@ -12,10 +11,11 @@ export default function Author(name: string) {
     return (
         <div>
             <div className="flex">
-                <Image src={`/images/author/${name}`} alt={name} width={30} height={30} className="justify-center border rounded-full" />
-                <h3 className="text-2xl">{name}</h3>
+                <Image src={`/images/author/${name}`} alt={name} width={10} height={10} className="justify-center border rounded-full" />
+                <a href={authors[name].url}>
+                    <h3 className="text-2xl content-center">{name}</h3>
+                </a>
             </div>
-            <p className="pt-3">{authors[name].introduce}</p>
         </div>
     )
 }
