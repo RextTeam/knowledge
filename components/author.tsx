802444353683099af0078f377bb5c94d13e9001e
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-28 11:00:15 +0900

    Update author.tsx

--- components/author.tsx
+++ components/author.tsx
@@ -23,7 +23,7 @@ export default function Author({ name }: Props) {
             <div className="flex">
                 <Image src={`/images/author/${name}`} alt={name} width={10} height={10} className="justify-center border rounded-full" />
                 <a href={authors[name].url}>
-                    <h3 className="text-2xl content-center">{name}</h3>
+                    <small className="text-xl content-center">{name}</small>
                 </a>
             </div>
         </div>
