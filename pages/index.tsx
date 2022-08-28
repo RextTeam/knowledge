51e905c34efab992e2e08fe91460b2806fca12e4
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-29 08:20:18 +0900

    delete: 余分な部分を削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
