723331cbdd22625f260622a300d71b303bc4cd1c
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-31 22:55:46 +0900

    Create ads.tsx

---
+++ components/ads.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+
+
+export function AdArticle() {
+    useEffect(() => {
+        (adsbygoogle = window.adsbygoogle || []).push({})
+    })
+    return (
+        <ins className="adsbygoogle"
+             style="display:block; text-align:center;"
+             data-ad-layout="in-article"
+             data-ad-format="fluid"
+             data-ad-client="ca-pub-8715826596324259"
+             data-ad-slot="3776860539"></ins>
+    )
+}
