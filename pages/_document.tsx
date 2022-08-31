d722cc00e02d94f9c73d8e2c34257ac632e1f33a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-31 20:26:22 +0900

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8715826596324259" crossorigin="anonymous"></script>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
