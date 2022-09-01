80d2493b9c210c6f9499c2ddbdd33a8ea14a6a21
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-01 15:22:38 +0900

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+            <h2 className="text-2xl pb-4">Notfound</h2>
+            <p>どうやら君は迷子になってしまったようだ。</p>
+        </>
+    )
+}
