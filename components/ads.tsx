b7e84177685d9bd9dcfec1e0472a864fce2bb786
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-01 13:10:40 +0900

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -14,7 +14,7 @@ export function AdArticle() {
         }
     }, [])
     return (
-        <ins className="block text-center py-2 adsbygoogle"
+        <ins className="block text-center py-4 adsbygoogle"
              data-ad-layout="in-article"
              data-ad-format="fluid"
              data-ad-client="ca-pub-8715826596324259"
