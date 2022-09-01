499ae39fe8a232a019042b533897bde900b5e969
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-01 09:31:45 +0000

    ok

--- components/ads.tsx
+++ components/ads.tsx
@@ -25,3 +25,22 @@ export function AdArticle() {
              data-ad-slot="3776860539"></ins>
     )
 }
+
+export function Ad() {
+    useEffect(() => {
+        if (typeof window !== undefined) {
+            try {
+                (window.adsbygoogle = window.adsbygoogle || []).push({})
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }, [])
+    return (
+        <ins className="block adsbygoogle"
+             data-ad-client="ca-pub-8715826596324259"
+             data-ad-slot="9147556969"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+    )
+}
