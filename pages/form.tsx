c619b81f37d62ff8675ebdb2474d17c2b219489b
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-04 19:05:25 +0900

    Update form.tsx

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,6 +1,6 @@
 export default function Form() {
     <>
         <h2 className="text-2xl">お問い合わせフォーム</h2>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScK1hpnxaVX-ceP6f25Aw40w5iLGHNvFIKS0np2TYdjFukb9A/viewform?embedded=true" width="640" height="844" frameBorder="0" marginHeight="0" marginWidth="0">読み込んでいます…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScK1hpnxaVX-ceP6f25Aw40w5iLGHNvFIKS0np2TYdjFukb9A/viewform?embedded=true" width={640} height={844} frameBorder={0} marginHeight={0} marginWidth={0}>読み込んでいます…</iframe>
     </>
 }
