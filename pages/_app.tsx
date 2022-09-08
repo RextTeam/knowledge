af0f3b6572536267c1b900d135fcc38a43e31c24
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-08 20:48:59 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import { useEffect } from 'react'
-import initTwitterScriptInner from 'zenn-embed-elements/lib/init-twitter-script-inner';
+import initTwitterScriptInner from 'zenn-embed-elements/lib/init-twitter-script-inner'
 
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(()=> import("zenn-embed-elements"),[])
