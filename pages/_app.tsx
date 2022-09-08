6cc06a06f0999dc7b8ac2ba879732d2e2adc82ed
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-08 20:53:44 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
-import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    useEffect(()=> import("zenn-embed-elements"),[])
-
     return (
         <Layout>
             <Component {...pageProps} />
