c7360ee2ed70e86b5017f476dfe7ddff55f38886
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-09 11:41:02 +0900

    Create new-form.tsx

---
+++ pages/new-form.tsx
@@ -0,0 +1,11 @@
+export default function NewForm() {
+    return (
+        <>
+            <h2 className="text-2xl">お問い合わせフォーム</h2>
+            <form method="post" action="https://hyperform.jp/api/kzhZu7sN">
+                <div>
+                </div>
+            </form>
+        </>
+    )
+}
