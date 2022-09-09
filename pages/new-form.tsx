b11c40005e192a248a740cda27502bf3dd8b3db9
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-09 12:08:55 +0900

    Update new-form.tsx

--- pages/new-form.tsx
+++ pages/new-form.tsx
@@ -4,6 +4,8 @@ export default function NewForm() {
             <h2 className="text-2xl">お問い合わせフォーム</h2>
             <form method="post" action="https://hyperform.jp/api/kzhZu7sN">
                 <div>
+                    <label>メールアドレス</label>
+                    <input type="email" name="Email" required />
                 </div>
             </form>
         </>
