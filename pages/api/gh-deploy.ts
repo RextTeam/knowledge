29ca2528225ec14224f7ee53f1c8aa580d474c03
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-10 15:52:28 +0900

    Update gh-deploy.ts

--- pages/api/gh-deploy.ts
+++ pages/api/gh-deploy.ts
@@ -2,9 +2,17 @@ import { NextApiResponse, NextApiRequest } from 'next'
 import { execSync } from 'child_process'
 
 
-export default function handler(_req: NextApiRequest, res: NextApiResponse<Person[]>) {
-    execSync("git pull origin main")
-    execSync("yarn build")
-    execSync("sudo systemctl start --now knowledge")
-    res.status(200).json({status: 200})
+type ResponseT = {
+    status: number,
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseT>) {
+    if (req.query.token === process.env.GH_DEPLOY_PASS) {
+        execSync("git pull origin main")
+        execSync("yarn build")
+        execSync("sudo systemctl start --now knowledge")
+        res.status(200).json({status: 200})
+    } else {
+        res.status(403).json({status: 403})
+    }
 }
